619e7b7bd1d929d0c9f1edd3f731b606a9373c5e
Alexey Antonov <alexeyantonov@users.noreply.github.com>
2018-04-24 20:59:51 +0300

    Update and rename run_jn.sh to run_img.sh

--- run_jn.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Usage: ./run_jn.sh <docker image name>
-docker run -p 8888:8888 -v `pwd`:/root --rm -it ${1}  jupyter notebook --ip 0.0.0.0 --no-browser --allow-root --NotebookApp.token=''
